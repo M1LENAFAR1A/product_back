631f6a4878ff475cb587a3e0ef183ad3c5e670cd
Milena <84186837+M1LENAFAR1A@users.noreply.github.com>
2025-01-11 17:10:33 -0300

    add img bd

--- db.js
+++ db.js
@@ -1,9 +1,31 @@
-// db.js
+// // db.js
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize('postgres', 'postgres', '12345', {
+//     host: 'localhost',
+//     dialect: 'postgres' // Altere para 'postgres' se estiver usando PostgreSQL
+// });
+
+// sequelize.authenticate()
+//     .then(() => console.log('Conexão com banco bem-sucedida!'))
+//     .catch(err => console.error('Erro ao conectar ao banco:', err));
+
+// module.exports = sequelize;
+
+require('dotenv').config(); // Adicionado no início do arquivo
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres', 'postgres', '12345', {
-    host: 'localhost',
-    dialect: 'postgres' // Altere para 'postgres' se estiver usando PostgreSQL
+// Variáveis de ambiente
+const database = process.env.DB_NAME || 'postgres';
+const username = process.env.DB_USER || 'postgres';
+const password = process.env.DB_PASS || '12345';
+const host = process.env.DB_HOST || 'localhost';
+const dialect = process.env.DB_DIALECT || 'postgres';
+
+const sequelize = new Sequelize(database, username, password, {
+    host,
+    dialect
 });
 
 sequelize.authenticate()
--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,35 @@
-// models/Product.js
+// // models/Product.js
+// const { DataTypes } = require('sequelize');
+// const sequelize = require('../db');
+
+// const Product = sequelize.define('Product', {
+//     id: {
+//         type: DataTypes.INTEGER,
+//         autoIncrement: true,
+//         primaryKey: true
+//     },
+//     name: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//     },
+//     type: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//     },
+//     price: {
+//         type: DataTypes.FLOAT,
+//         allowNull: false
+//     },
+//     qtd: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false
+//     }
+// }, {
+//     timestamps: true // Cria as colunas createdAt e updatedAt automaticamente
+// });
+
+// module.exports = Product;
+
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db');
 
@@ -23,6 +54,10 @@ const Product = sequelize.define('Product', {
     qtd: {
         type: DataTypes.INTEGER,
         allowNull: false
+    },
+    image: { // Nova coluna para armazenar a URL da imagem
+        type: DataTypes.STRING,
+        allowNull: true
     }
 }, {
     timestamps: true // Cria as colunas createdAt e updatedAt automaticamente
